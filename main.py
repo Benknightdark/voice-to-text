72dcedca75d110a35bc8ae8b29f6b50abb11d1b1
BenKung <moon791017@gmail.com>
2021-09-28 17:14:29 +0800

    OK

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tempfile import TemporaryFile
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 def convert_text_speach():
-    tts = gTTS('我不明白你的問題，請再重複一次', lang='zh')
+    tts = gTTS('我不明白你的問題，請再重複一次', lang='zh-tw')
     f = TemporaryFile()
     tts.write_to_fp(f)
     f.seek(0)
