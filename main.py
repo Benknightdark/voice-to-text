5f4d99d86aa4452e3245d28702662d86911fc3c7
BenKung <moon791017@gmail.com>
2021-09-28 17:09:18 +0800

    OK

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from gtts import gTTS
-tts = gTTS('我不明白你的問題，請再重複一次', lang='zh')
-tts.save('hello.mp3')
-tts2=gTTS('好的，現在時間是2月2號，請繫上安全帶，我們即將前往8D時空。', lang='zh')
-tts2.save('好的現在時間是2月2號請繫上安全帶我們即將前往8D時空.mp3')
+from tempfile import TemporaryFile
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+def convert_text_speach():
+    tts = gTTS('我不明白你的問題，請再重複一次', lang='zh')
+    f = TemporaryFile()
+    tts.write_to_fp(f)
+    f.seek(0)
+    return f
+
+
+
+some_file_path = "large-video-file.mp4"
+app = FastAPI()
+
+
+@app.get("/")
+async def main():
+    return  StreamingResponse(convert_text_speach(), media_type="audio/mpeg")
